893c785fe8e4689c1034c563e355e7facb36d74c
Anant Paul <14359shanant@gmail.com>
2026-07-20 16:15:14 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import base64
 import cv2
+import os
 
 app = Flask(__name__)
 
@@ -41,4 +42,5 @@ def predict():
     return jsonify({'prediction': label})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
