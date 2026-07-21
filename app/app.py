6a2cf6a3e20d07d18fb5a89395e0d86579376406
Anant Paul <14359shanant@gmail.com>
2026-07-21 19:58:32 +0530

    Update prediction method to use landmarks
    Refactor prediction logic to use pre-sent landmarks instead of image processing.

--- app/app.py
+++ app/app.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-import mediapipe as mp
 import pickle
-import numpy as np
-import base64
-import cv2
 import os
 
 app = Flask(__name__)
@@ -13,32 +9,15 @@ with open('../models/isl_model.pkl', 'rb') as f:
 with open('../models/label_encoder.pkl', 'rb') as f:
     le = pickle.load(f)
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json['image']
-    img_data = base64.b64decode(data.split(',')[1])
-    np_arr = np.frombuffer(img_data, np.uint8)
-    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    result = hands.process(rgb)
-
-    label = ""
-    if result.multi_hand_landmarks:
-        hand_landmarks = result.multi_hand_landmarks[0]
-        coords = []
-        for lm in hand_landmarks.landmark:
-            coords.extend([lm.x, lm.y, lm.z])
-        pred = model.predict([coords])[0]
-        label = le.inverse_transform([pred])[0]
-
+    coords = request.json['landmarks']
+    pred = model.predict([coords])[0]
+    label = le.inverse_transform([pred])[0]
     return jsonify({'prediction': label})
 
 if __name__ == '__main__':
